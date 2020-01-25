89310f3d9e48f7d678d2233a2f6012c9548cbb08
Milica Travica <mica97@email.com>
2020-01-25 14:00:19 +0100

    init editor

--- xmlScientifcPublicationEditorFront/src/app/publication/add-publication/add-publication.component.ts
+++ xmlScientifcPublicationEditorFront/src/app/publication/add-publication/add-publication.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare const Xonomy: any;
 
 @Component({
   selector: 'app-add-publication',
@@ -10,6 +11,58 @@ export class AddPublicationComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const docSpec = {
+      onchange: function () {
+        console.log('I been changed now!')
+      },
+      validate: function (obj) {
+        console.log('I be validatin\' now!')
+      },
+      elements: {
+        list: {
+          menu: [{
+            caption: 'Append an <item>',
+            action: Xonomy.newElementChild,
+            actionParameter: '<item/>'
+          }]
+        },
+        item: {
+          menu: [{
+            caption: 'Add @label="something"',
+            action: Xonomy.newAttribute,
+            actionParameter: { name: 'label', value: 'something' },
+            hideIf: function (jsElement) {
+              return jsElement.hasAttribute('label');
+            }
+          }, {
+            caption: 'Delete this <item>',
+            action: Xonomy.deleteElement
+          }, {
+            caption: 'New <item> before this',
+            action: Xonomy.newElementBefore,
+            actionParameter: '<item/>'
+          }, {
+            caption: 'New <item> after this',
+            action: Xonomy.newElementAfter,
+            actionParameter: '<item/>'
+          }],
+          canDropTo: ['list'],
+          attributes: {
+            label: {
+              asker: Xonomy.askString,
+              menu: [{
+                caption: 'Delete this @label',
+                action: Xonomy.deleteAttribute
+              }]
+            }
+          }
+        }
+      }
+    };
+    const xml = '<list><item label=\'one\'/><item label=\'two\'/></list>';
+    const editor = document.getElementById('ecitor');
+    // tslint:disable-next-line: no-unused-expression
+    new Xonomy.render(xml, editor, docSpec);
   }
 
 }
